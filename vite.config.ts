a11157dd8c34af5ef9feaf545784638ff3afb7e2
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2023-09-06 16:11:14 +0800

    Set up Chrome extension with Hot Reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { crx } from "@crxjs/vite-plugin";
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    crx({
+      manifest
+    })
   ],
   resolve: {
     alias: {
