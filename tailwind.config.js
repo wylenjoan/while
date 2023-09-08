0d056ab3093e500432a7209fd52b7ebbafff0934
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2023-09-08 10:16:05 +0800

    Set up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,ts,tsx,vue}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
