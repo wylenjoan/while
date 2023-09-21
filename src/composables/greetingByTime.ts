5fc43f179cee77f45775ea582341663f5c311f8f
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2023-09-21 16:39:02 +0800

    Fix type error

--- src/composables/greetingByTime.ts
+++ src/composables/greetingByTime.ts
@@ -2,7 +2,8 @@ import greetings from "@/assets/greetings.json"
 import useRandomNumberInRange from "./randomNumberInRange"
 
 export default function useGreetingByTime(dayPart: string): string {
-  const currentGreetings = greetings[dayPart]
+  const greetingsObj: Record<string, string[]> = greetings;
+  const currentGreetings = greetingsObj[dayPart]
   const randomNumber = useRandomNumberInRange(0, currentGreetings.length)
 
   return currentGreetings[randomNumber] ?? "Hello"
